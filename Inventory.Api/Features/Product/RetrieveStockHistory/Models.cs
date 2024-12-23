37f4e401df99833c1ee0866754de88a940240196
Marvin Thomaz do Nascimento <marvin.nascimento@invillia.com>
2024-12-23 10:28:33 -0300

    adiciona validação de id do produto na busca do historico

--- Inventory.Api/Features/Product/RetrieveStockHistory/Models.cs
+++ Inventory.Api/Features/Product/RetrieveStockHistory/Models.cs
@@ -1,8 +1,21 @@
-﻿namespace Inventory.Api.Features.Product.RetrieveStockHistory
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace Inventory.Api.Features.Product.RetrieveStockHistory
 {
     internal sealed class Request
     {
         public string Id { get; set; }
+    }
+
+    internal sealed class Validator : Validator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Id do produto é obrigatório");
+        }
     }
 
     internal sealed class Response
